c7253b167a87f91bc31abea11eb76a2189804693
Mikalai Mitsin <mikalai.mitsin@elasticsoft.io>
2022-12-08 22:28:13 +0100

    feat: add cache

--- src/main/kotlin/com/018bf/plugins/Routing.kt
+++ src/main/kotlin/com/018bf/plugins/Routing.kt
@@ -4,6 +4,8 @@ import com.`018bf`.interfaces.space.MessageHandler
 import com.`018bf`.repositories.IssueRepository
 import com.`018bf`.usecases.ReportUseCase
 import io.ktor.http.*
+import io.ktor.client.plugins.cache.*
+
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
@@ -17,7 +19,9 @@ fun Application.configureRouting() {
     val clientId = environment.config.property("ktor.space.clientId").getString()
     val clientSecret = environment.config.property("ktor.space.clientSecret").getString()
     val spaceServerUrl = environment.config.property("ktor.space.spaceServerUrl").getString()
-    val spaceHttpClient = ktorClientForSpace()
+    val spaceHttpClient = ktorClientForSpace {
+        install(HttpCache)
+    }
     val spaceAppInstance = SpaceAppInstance(
         clientId = clientId,
         clientSecret = clientSecret,
